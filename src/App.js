d514ce844a3b8e69e9b99021abffcaa67fe1c0aa
trev4ev <trevoraquino@gmail.com>
2019-03-14 21:33:16 -0700

    add toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,75 @@
-import React, { Component } from "react";
-import "./App.css";
-import Item from "./Item";
+// @format
+import React, {Component} from 'react';
+import './App.css';
+import Item from './Item';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      curr_item: "",
-      items: []
+      curr_item: '',
+      items: [],
     };
     this.handleItem = this.handleItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.toggleDone = this.toggleDone.bind(this);
   }
   handleItem(event) {
     this.setState({
-      curr_item: event.target.value
+      curr_item: event.target.value,
     });
   }
   addItem() {
-    if(this.state.curr_item !== ''){
+    if (this.state.curr_item !== '') {
       this.setState({
-        curr_item: "",
-        items: this.state.items.concat(this.state.curr_item)
+        curr_item: '',
+        items: this.state.items.concat({
+          text: this.state.curr_item,
+          done: false,
+        }),
       });
     }
+  }
+  toggleDone(index) {
+    let oldList = this.state.items;
+    oldList[index].done = !oldList[index].done;
+    this.setState({
+      items: oldList,
+    });
+  }
+  countWrong() {
+    let count = 0;
+    for (let i in this.state.items) {
+      if (!this.state.items[i].done) {
+        count++;
+      }
+    }
+    return count;
   }
   render() {
     return (
       <div className="todo-container">
         <div className="input-container">
-          <input className="todo-input" placeholder="New Item" value={this.state.curr_item} onChange={this.handleItem} />
-          <button className="todo-submit" onClick={this.addItem}>Submit</button>
+          <input
+            className="todo-input"
+            placeholder="New Item"
+            value={this.state.curr_item}
+            onChange={this.handleItem}
+          />
+          <button className="todo-submit" onClick={this.addItem}>
+            Submit
+          </button>
         </div>
         <ul className="todo-item-list">
           {this.state.items.map((item, index) => (
-            <Item key={index} text={item} />
+            <Item
+              index={index}
+              toggleDone={this.toggleDone}
+              key={index}
+              done={item.done}
+              text={item.text}
+            />
           ))}
+          <li style={{fontWeight: 600}}>{this.countWrong()} items left!</li>
         </ul>
       </div>
     );
--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,25 @@
-import React, { Component } from "react";
+// @format
+import React, {Component} from 'react';
 
 class Item extends Component {
   render() {
+    let index = this.props.index;
     return (
-        <li> {this.props.text} </li>
+      <div>
+        {this.props.done ? (
+          <li
+            style={{textDecoration: 'line-through'}}
+            onClick={() => this.props.toggleDone(index)}>
+            {' '}
+            {this.props.text}{' '}
+          </li>
+        ) : (
+          <li onClick={() => this.props.toggleDone(index)}>
+            {' '}
+            {this.props.text}{' '}
+          </li>
+        )}
+      </div>
     );
   }
 }
